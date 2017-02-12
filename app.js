1b36cbb86d72125da9f281f7b1bcb5a5860428a9
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-12 03:41:34 +0000

    added app for webpage rendering

---
+++ app.js
@@ -0,0 +1,60 @@
+/* app.js */
+
+// require and instantiate express
+const app = require('express')()
+
+// fake posts to simulate database
+const posts = [
+  {
+    id:1,
+    author: 'John',
+    title: 'Templating with EJS',
+    body: 'Blog post number 1'
+  },
+  {
+    id: 2,
+    author: 'Drake',
+    title: 'Express: Starting from the Bottom',
+    body: 'Blog post number2'
+  },
+  {
+    id: 3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 3'
+  },
+  {
+    id: 4,
+    author: 'Cody',
+    title: 'Events',
+    body: 'Blog post number 4'
+  }
+]
+
+// ste the view engine to ejs
+app.set('view engine', 'ejs')
+
+// blog home page
+app.get('/', (req, res) => {
+  // render `home.ejs` with the list of posts
+  res.render('home', { posts: posts })
+})
+
+// blog post
+app.get('/post/:id', (req, res) => {
+  // find the post in the `posts` array
+  const post = posts.filter((post) => {
+    return post.id == req.params.id
+  })[0]
+
+  // render the `post.ejs` template with the post content
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  })
+})
+
+app.listen(8080)
+
+console.log('listening on port 8080')
